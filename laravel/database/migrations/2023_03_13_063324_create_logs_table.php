6f982dc32e368cd378b1eb4806e457627a3ecfa0
unknown <wetemez@gmail.com>
2023-03-13 10:16:30 +0300

    Create migrations

---
+++ laravel/database/migrations/2023_03_13_063324_create_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('userId');
+            $table->string('description');
+
+            $table->foreign('userId')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+};
---
+++ laravel/database/migrations/2023_03_13_063427_create_remove_file_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('remove_file', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('storageId');
+            $table->string('path');
+            $table->unsignedBigInteger('typeId');
+
+            $table->foreign('typeId')->references('id')->on('file_type');
+            $table->foreign('storageId')->references('id')->on('storage');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('remove_file');
+    }
+};
